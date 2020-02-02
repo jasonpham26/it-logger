8be013c6fdc367a9658d7a8783f0340a3adb23e3
jason <vinhphuong0206@gmail.com>
2020-02-02 22:33:21 +1000

    update components

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
   return (
-    <li className='collection-item'>
+    <li className="collection-item">
       <div>
         <a
-          href='#edit-log-modal'
+          href="#edit-log-modal"
           className={`modal-trigger ${
             log.attention ? 'red-text' : 'blue-text'
           }`}
@@ -15,13 +15,13 @@ const LogItem = ({ log }) => {
           {log.message}
         </a>
         <br />
-        <span className='grey-text'>
-          <span className='black-text'>ID: #{log.id}</span> last updated by{' '}
-          <span className='black-text'>{log.tech}</span> on{' '}
-          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+        <span className="grey-text">
+          <span className="black-text">ID: #{log.id}</span> last updated by{' '}
+          <span className="black-text">{log.tech}</span> on{' '}
+          <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
-        <a href='' className='secondary-content'>
-          <i className='material-icons grey-text'>delete</i>
+        <a href="#!" className="secondary-content">
+          <i className="material-icons grey-text">delete</i>
         </a>
       </div>
     </li>
@@ -29,7 +29,7 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
 };
 
 export default LogItem;
---
+++ src/reducers/logReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action) {
+    default:
+      return state;
+  }
+};
